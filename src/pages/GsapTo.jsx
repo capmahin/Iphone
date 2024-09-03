c5f83ceccc1531b6de039f7038b0f6750039bc82
capmahin <mahinulhuque1996@gmail.com>
2024-09-03 13:51:36 +0600

    target blue box

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,11 +1,12 @@
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
 
   useGSAP(()=>{
-    
-  })
+     gsap.to('#blue-box')
+  },[]);
 
   return (
     <main>
