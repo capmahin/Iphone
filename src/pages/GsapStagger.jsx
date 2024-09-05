c441605bed40b9a0493ab12f9967f7661980e226
capmahin <mahinulhuque1996@gmail.com>
2024-09-05 23:22:20 +0600

    add more propertyis

--- src/pages/GsapStagger.jsx
+++ src/pages/GsapStagger.jsx
@@ -11,7 +11,14 @@ const GsapStagger = () => {
       borderRadius:'100%',
       repeat:-1,
       yoyo:true,
-      stagger:0.5,
+      // stagger:0.5,
+      stagger:{
+        amount:1.5,
+        grid:[2,1],
+        axis:'y',
+        ease:'circ.inOut',
+        from:'center'
+      }
     })
   },[])
   return (
